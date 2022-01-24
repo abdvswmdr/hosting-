46950ac88763e12443fa29dcd68c2e02bc5d3c73
Abdulswamad Rama <67716879+abdvswmdr@users.noreply.github.com>
2022-01-24 17:21:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
-import praw
+import praw 
 
 reddit = praw.Reddit(
-    client_id="aVib-yPvkf_N72ZGeJS4cQ",
-    client_secret="Ar4xkXyjcOTFuYop51bZnn6l-N5idA",
-    password= "Hehehe12345.",
-    user_agent="<console:HAPPY:1.0>",
-    username = "abdvswmdr",
- )
-
-
-
+    client_id = "7i66SjwRcRKHNNaSlRr1GA", 
+    client_secret = "adBGWPi8VW6rXpefr_iJC9o4qIazfQ", 
+    password = "Hehehe12345.", 
+    user_agent = "<console:HAPPY:1.0", 
+    username = "Abdvswmdr-2047"
+    
+    )
 
 import random
-import time
-def karma():
+import time 
+def karma(): 
     try:
-        messages = ["Upvoted, upvote in return?",
-                "Great post, care to share the upvotes!"]
-        for submission in reddit.subreddit("FreeKarma4U+FreeKarma4You").stream.submissions(): 
+        messages = ["Upvoted! Upvote in return?", 
+                    "Great post, care to share the upvotes!", 
+                    "Just upvoted you, would you mind upvoting in return!"]
+        for submission in reddit.subreddit("FreeKarma4U+FreeKarma4You").stream.submissions():
             submission.upvote()
             rand = random.randint(0, (len(messages)-1))
             print(submission.title)
-            submission.reply("Upvoted, upvote in return?")
+            submission.reply(messages[rand])
             time.sleep(80)
-    except: 
-        time.sleep(300)
+    except:
+        time.sleep(500)
         karma()
+karma()
+                
